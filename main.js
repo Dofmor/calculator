3e2cf6f08d57b1bdfdb999e722b84afd130a57c4
Daniel Morales <danny.mora542@gmail.com>
2023-09-04 20:13:29 -0700

    Finish implementing percentage and inverse buttons

--- main.js
+++ main.js
@@ -17,6 +17,11 @@ function clickButtons() {
             setOperator(button.innerHTML)
         else if (button.id === 'clear')
             clear()
+        else if (button.id === 'invert')
+            invert()
+        else if (button.id === 'percentage')
+            percentage()
+
     }))
 }
 
@@ -37,24 +42,25 @@ function setNumber(value) {
         secondNumber += value
         updateDisplay(secondNumber)
     }
+    console.log(`firstNumber: ${firstNumber} secondNumber: ${secondNumber} operator: ${operator}`)
 }
 
 function setOperator(value) {
+    firstValueSet = true
     if (operator !== '') {
-        evaluate()        
+        evaluate()
         operator = value
         return
-    } 
-    else if (firstNumber === '') 
+    }
+    else if (firstNumber === '')
         firstNumber = '0'
     operator = value
-    firstValueSet = true
     updateDisplay(operator)
 }
 
 function evaluate() {
 
-    if(secondNumber === '')
+    if (secondNumber === '')
         return
 
     console.log(`firstNumber: ${firstNumber} secondNumber: ${secondNumber} operator: ${operator}`)
@@ -91,4 +97,37 @@ function clear() {
     firstNumber = secondNumber = ''
     firstValueSet = false
     updateDisplay('0')
+}
+
+function invert() {
+    let invertedVal
+    if (!firstValueSet && firstNumber !== '') {
+        (firstNumber > 0) ? invertedVal = "-" + firstNumber : invertedVal = firstNumber.substring(1)
+        firstNumber = invertedVal
+        updateDisplay(firstNumber)
+    }
+    else if (secondNumber !== '') {
+        (secondNumber > 0) ? invertedVal = "-" + secondNumber : invertedVal = secondNumber.substring(1)
+        secondNumber = invertedVal
+        updateDisplay(secondNumber)
+    }
+
+}
+
+function percentage() {
+    let percentagedVal
+
+    if (!firstValueSet && firstNumber !== '' && firstNumber !== '0'){
+        percentagedVal = parseFloat(firstNumber)/100
+        firstNumber = '' + percentagedVal
+        updateDisplay(firstNumber)
+    }
+    else if (secondNumber !== '' && secondNumber !== '0'){
+        percentagedVal = parseFloat(SecurityPolicyViolationEvent)/100
+        secondNumber = '' + percentagedVal
+        updateDisplay(secondNumber)
+    }
+
+
+        
 }
